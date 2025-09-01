9e5abc3b1b31ec871ccdf7260ae51167df884032
Waldo <waldito.07@gmail.com>
2025-09-01 22:04:53 +0000

    Serve static assets at /public via express.static

--- myApp.js
+++ myApp.js
@@ -4,9 +4,15 @@ const app = express();
 
 console.log("Hello World");
 
+// 1) serve static assets from /public
+//  Requests like GET /public/style.css will be served from the /public folder.
+app.use('/public', express.static(path,join(__dirname, 'public')));
+
+
 // If you still have a previous root route like this, comment it out:
 // app.get("/", (req, res) => res.send("Hello Express"));
 
+// 2) Root route -> send index.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "index.html"));
 });
