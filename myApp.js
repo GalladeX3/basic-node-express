099f1893497078c13311d1279ce81301426fa6cd
Waldo <waldito.07@gmail.com>
2025-09-01 21:57:28 +0000

    Serve views/index.html at / with res.sendFile

--- myApp.js
+++ myApp.js
@@ -1,7 +1,17 @@
-let express = require('express');
-let app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
 
 console.log("Hello World");
+
+// If you still have a previous root route like this, comment it out:
+// app.get("/", (req, res) => res.send("Hello Express"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+
+module.exports = app;
 
 
 
